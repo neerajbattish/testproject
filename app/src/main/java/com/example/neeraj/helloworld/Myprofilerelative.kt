ea83fc5964738e989e3b75ef94545628fd9573d1
Neeraj Battish <neerajbattish@yahoo.com>
2019-10-06 14:12:30 +0530

    my commit

---
+++ app/src/main/java/com/example/neeraj/helloworld/Myprofilerelative.kt
@@ -0,0 +1,12 @@
+package com.example.neeraj.helloworld
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Myprofilerelative : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_myprofilerelative)
+    }
+}
